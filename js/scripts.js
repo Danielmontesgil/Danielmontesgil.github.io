8b51d4dd9df41a3e2daf856d19d7612dd27cb522
Daniel Montes Gil <dmontesgil@gmail.com>
2024-10-01 19:17:01 +0200

    scripts file name changed

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,7 @@ window.onclick = function(event) {
     }
 }
 
+// Desplazamiento suave entre secciones
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
